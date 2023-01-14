9e4f23d5b9eae9faf6c52125d319276f7a91b20e
zzp_miracle <zhangzipeng.zzp@alibaba-inc.com>
2023-01-14 19:12:38 +0800

    fix for torchdynamo fake tensor mode tentatively

--- torchbenchmark/util/backends/torchdynamo.py
+++ torchbenchmark/util/backends/torchdynamo.py
@@ -56,6 +56,7 @@ def parse_torchdynamo_args(model: 'torchbenchmark.util.model.BenchmarkModel', dy
 
 def apply_torchdynamo_args(model: 'torchbenchmark.util.model.BenchmarkModel', args: argparse.Namespace, precision: str):
     # torchdynamo.config.suppress_errors = True
+    torchdynamo.config.DO_NOT_USE_legacy_non_fake_example_inputs = True
     torchdynamo.reset()
     torchdynamo.utils.counters.clear()
 
